f3c174343720fca90b5ee02cb71d040e1000cd6c
Mr Rajan <84144360+rajan-great@users.noreply.github.com>
2025-06-23 19:17:35 +0530

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,1 +1,11 @@
- 
+const express = require('express');
+const router = express.Router();
+const cartController = require('../controllers/cartController');
+const auth = require('../middleware/authMiddleware');
+
+router.get('/', auth, cartController.getCart);
+router.post('/', auth, cartController.addToCart);
+router.put('/:itemId', auth, cartController.updateCart);
+router.delete('/:itemId', auth, cartController.removeFromCart);
+
+module.exports = router; 
